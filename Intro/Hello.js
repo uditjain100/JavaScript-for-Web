5950eae276b5ed22b2622ae2a54e1b26cf0ed84d
Udit Jain <jain30udit@gmail.com>
2021-01-15 19:22:11 +0530

    first commit

---
+++ Intro/Hello.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log(2 + 1);
+console.log(typeof NaN);
+console.log(400 + 0 / 0);
+var i = 5;
+if (i == 5) console.log("yeaaaaa!!");
+let input = prompt("Enter a Number");
+if (input == 5) console.log("qwertyuio");
+else console.log("qwertyuio");
+let arr = [1, 2, 4, 6, 2, 5, 7, 3, 5, 1, 2, 8, 4, 1, 2, 6, 5];
+arr.sort();
+console.log(arr);
