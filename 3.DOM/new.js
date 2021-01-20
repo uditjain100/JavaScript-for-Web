26c8c94ec8006569943afb3409d03ce64ffc8ea4
Udit Jain <jain30udit@gmail.com>
2021-01-20 17:57:14 +0530

    baisc DOM

--- 3.DOM/new.js
+++ 3.DOM/new.js
@@ -10,7 +10,38 @@ var allLinks = document.querySelectorAll("a");
 
 for (var link of allLinks) console.log(link.href);
 
-var doneTodos = document.querySelectorAll(".done");
-var checkbox = document.querySelector('input[id="scales"]');
+// var doneTodos = document.querySelectorAll(".done");
+// var checkbox = document.querySelector('input[id="scales"]');
 
-for (var ele of donetodos) console.log(ele);
+// for (var ele of donetodos) console.log(ele);
+
+var text = document.querySelector("p").innerText;
+console.log(text);
+var html = document.querySelector("p").innerHTML;
+console.log(html);
+
+document.querySelector("h1 span").innerText = "Disgusting";
+
+for (var link of allLinks) link.style.borderRadius = "red";
+
+console.log("Done");
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+var spanAll = document.querySelectorAll("span");
+var i = 0;
+for (var ele of spanAll) ele.style.color = colors[i++];
+
+document.querySelector("h2").classList.add("border");
+document.querySelector("h2").classList.add("border");
+
+var list = document.querySelectorAll("li");
+for (var ele of list)
+  if (ele.className === "highlight") ele.classList.toggle("purple");
+
+// WRITE YOUR CODE IN HERE:
+for (var i = 0; i < 100; i++) {
+  var newb = document.createElement("button");
+  newb.innerText = "Hey!";
+  document.querySelector("#container").appendChild(newb);
+}
