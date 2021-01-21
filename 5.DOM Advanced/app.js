c793ee70ae104a811521154482789f7c51272324
Udit Jain <jain30udit@gmail.com>
2021-01-21 13:09:08 +0530

    events

---
+++ 5.DOM Advanced/app.js
@@ -0,0 +1,18 @@
+var btn = document.querySelector("#btn2");
+
+btn.onclick = function () {
+  console.log("Hey There, you clicked me !!");
+};
+
+btn.onmouseenter = () => {
+  console.log("Stop touching me");
+};
+
+var hab = document.querySelector("#hab");
+
+hab.addEventListener("mousedown", () => {
+  console.log("Hello there :)");
+});
+hab.addEventListener("mouseup", () => {
+  console.log("Good bye then :( ");
+});
