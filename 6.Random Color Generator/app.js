c592e499601a123dbd59ff8472602bdf91b0515f
Udit Jain <jain30udit@gmail.com>
2021-01-21 19:16:10 +0530

    rest

--- 6.Random Color Generator/app.js
+++ 6.Random Color Generator/app.js
@@ -1,14 +1,35 @@
-var body = document.querySelector("body");
-var btn = document.querySelector("#btn");
+var html = document.querySelector("html");
+var body = document.querySelector(".container");
+var btn = document.querySelector("#generate_btn");
 var dcf = document.querySelector("#display_color_field");
 
 var rand1, rand2, rand3;
 
-btn.addEventListener("mouseenter", () => {
+var changeColor = function () {
   rand1 = Math.floor(Math.random() * 256);
   rand2 = Math.floor(Math.random() * 256);
   rand3 = Math.floor(Math.random() * 256);
   body.style.backgroundColor = "rgb(" + rand1 + "," + rand2 + "," + rand3 + ")";
   console.log("rgb(" + rand1 + "," + rand2 + "," + rand3 + ")");
   dcf.innerText = "rgb(" + rand1 + "," + rand2 + "," + rand3 + ")";
-});
+};
+
+// html.addEventListener("mouseenter", changeFun);
+// body.addEventListener("mouseenter", changeFun);
+btn.addEventListener("click", changeColor);
+
+// for (var i = 0; i < 50; i++) {
+//   var newB = document.createElement("button");
+//   newB.innerText = "Click Me";
+//   body.appendChild(newB);
+// }
+var allbtns = document.querySelectorAll("#btn");
+var convertColor = function () {
+  rand1 = Math.floor(Math.random() * 256);
+  rand2 = Math.floor(Math.random() * 256);
+  rand3 = Math.floor(Math.random() * 256);
+  this.style.backgroundColor = "rgb(" + rand1 + "," + rand2 + "," + rand3 + ")";
+  //   this.style.color = "rgb(" + rand3 + "," + rand2 + "," + rand1 + ")";
+};
+
+for (var btn of allbtns) btn.addEventListener("click", convertColor);
